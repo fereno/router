46a90e6806b8693aa5e21ace57bc106b0db0491f
fereno <fereno1989@gmail.com>
2023-07-15 10:34:59 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from "./pages/Home";
+import Products from "./pages/Products";
+const router = createBrowserRouter([
+  {path: "/", element: <HomePage />},
+  {path: "/products", element: <Products />},
+]);
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/pages/Products.js
@@ -0,0 +1,4 @@
+function Products() {
+  return <h1>products page</h1>;
+}
+export default Products;
