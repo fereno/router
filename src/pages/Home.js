94e4a7c83dd091be23604491f2385c56ae3cde9e
fereno <fereno1989@gmail.com>
2023-07-15 11:43:06 +0330

    navigate between page with Link from "react-router-dom"

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,12 @@
+import {Link} from "react-router-dom";
 function HomePage() {
-  return <h1>HomePage ...</h1>;
+  return (
+    <>
+      <h1>HomePage ...</h1>
+      <p>
+        Go to Products page <Link to="/products">click here</Link>
+      </p>
+    </>
+  );
 }
 export default HomePage;
