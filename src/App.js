30cb1d69590c02c0ae93552148ca3a3affd33dbf
fereno <fereno1989@gmail.com>
2023-07-15 10:49:29 +0330

    createRoutesFromElements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import HomePage from "./pages/Home";
 import Products from "./pages/Products";
-const router = createBrowserRouter([
-  {path: "/", element: <HomePage />},
-  {path: "/products", element: <Products />},
-]);
+
+// const router = createBrowserRouter([
+//   {path: "/", element: <HomePage />},
+//   {path: "/products", element: <Products />},
+// ]);
+const routeDefinition = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<Products />} />
+  </Route>
+);
+const router = createBrowserRouter(routeDefinition);
 function App() {
   return <RouterProvider router={router} />;
 }
