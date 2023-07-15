4c44049103c3a4c516385c3da10ab9c3584061f2
fereno <fereno1989@gmail.com>
2023-07-15 14:19:37 +0330

    navigation & Error page

--- src/App.js
+++ src/App.js
@@ -6,18 +6,28 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Products from "./pages/Products";
+import RootLayout from "./pages/Root";
+import Error from "./pages/Error";
 
-// const router = createBrowserRouter([
-//   {path: "/", element: <HomePage />},
-//   {path: "/products", element: <Products />},
-// ]);
-const routeDefinition = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<Products />} />
-  </Route>
-);
-const router = createBrowserRouter(routeDefinition);
+// const routeDefinition = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// );
+//const router = createBrowserRouter(routeDefinition);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <Error />,
+    children: [
+      {path: "/", element: <HomePage />},
+      {path: "/products", element: <Products />},
+    ],
+  },
+]);
 function App() {
   return <RouterProvider router={router} />;
 }
